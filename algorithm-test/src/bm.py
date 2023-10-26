133285758c0271c8aa13d84a4525941c73b50813
Kenta Kudo <yd37574@gmail.com>
2023-10-27 08:29:08 +0900

    algorithm-test: naive impl

--- algorithm-test/src/bm.py
+++ algorithm-test/src/bm.py
@@ -11,7 +11,7 @@ class Bm(object):
     def __init__(self, text: str, pattern: str):
         self.text = text
         self.pattern = pattern
-        self.table = make_km_table(pattern)
+        # self.table = make_km_table(pattern)
 
     def decide_slide_width(self, c: str) -> int:
         assert len(c) == 1
@@ -19,5 +19,20 @@ class Bm(object):
         return -1
 
     def search(self) -> int:
-        raise Exception("TODO")
+        m = len(self.pattern)
+        n = len(self.text)
+        if m == 0 or n == 0 or n < m:
+            return -1
+        
+        for i in range(n - m + 1):
+            if self.match_at(i):
+                return i            
+
         return -1
+    
+    def match_at(self, i) -> bool:
+        m = len(self.pattern)
+        for j in range(m):
+            if self.text[i + j] != self.pattern[j]:
+                return False            
+        return True
